ee487bce6171d82f010412c4e3c09b93251be1a2
Nathan Piper <nathanpiper22@gmail.com>
2022-02-10 17:05:51 -0800

    added github, linkedin, and resume links to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,12 @@ function Header() {
         <h1 id="title">
           Nathan Piper <span id="title-desc">front-end web developer</span>
         </h1>
+        <ul className="header-links">
+            <li><a href="https://github.com/nather22" target="_blank"><img alt="github logo" src="https://img.icons8.com/ios-glyphs/120/000000/github.png" /> github</a></li>
+            <li><a href="" ><img alt="linkedin logo" src="https://img.icons8.com/ios-filled/100/000000/linkedin.png" /> linkedin</a></li>
+            <li><a href="" ><img alt="resume logo" src="https://img.icons8.com/ios/100/000000/open-resume.png" /> resume</a></li>
+            <li><a href="" ><img alt="work briefcase logo" src="https://img.icons8.com/dotty/80/000000/briefcase.png" /> my work</a></li>
+        </ul>
       </div>
       <Nav />
       <p className="scroll-down">
